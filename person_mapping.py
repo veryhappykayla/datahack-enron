5c3fca62e09fe8338ef3c8fffc538559a4557edf
GGGapp <galsdh@gmail.com>
2015-11-26 09:56:13 +0200

    person mapping script added and the map is added in json format

--- person_mapping.py
+++ person_mapping.py
@@ -1,59 +1,11 @@
-### connections ####
+####################
+## Person mapping ##
+####################
 
-## imports ##
-import networkx as nx
-import pylab
 import pickle
 from initial_data_parser import *
 import math
-
-class Connection(object):
-    def __init__(self, A, B, n_times = 0): # n_times is how many times A sent B a massege
-            self.graph = nx.Graph()
-            self.sender = A
-            self.reciver = B
-            self.n_times = n_times
-            self.ID = str(hash(A))+str(hash(B)) # for checking if there more than one connection between 2 people 
-
-    def inc(self):
-        self.n_times += 1
-
-    def get_ID(self):
-        return self.ID
-
-###########################
-#### Manipulating Data ####
-###########################
-    
-def build_graph(mails, start_date = None, end_date = None):
-    ALL_CONNECTIONS = {}
-    G = nx.DiGraph()
-    #filtering mails by time
-    if start_date != None and end_date != None: 
-        for mail in mails:
-            if (mail.headers['Date'] < start_date) or (mail.headers['Date'] > end_date):
-               mails.remove(mail) 
-    #combining all mail to graph
-    for mail in mails: 
-        new_connection = Connection(mail.headers['To'], mail.headers['From'])
-        if ALL_CONNECTIONS.has_key(new_connection.get_ID()):
-            ALL_CONNECTIONS[new_connection.get_ID()].inc()
-        else:
-            ALL_CONNECTIONS[new_connection.get_ID()] = new_connection
-            
-    for val in ALL_CONNECTIONS.values():
-        G.add_edge(val.sender, val.reciver, weight = val.n_times)
-    return G
-
-
-#########################
-## helpful functions ####
-#########################
-def print_graph(G):
-    #TODO
-    # need to print lables on the red dots
-    nx.draw(G)
-    pylab.show()
+import json
 
 def out(lst):
     for i in range(len(lst)):
@@ -114,13 +66,4 @@ def map_person_to_mails(person):
             for i in M:
                 if M[i] > n + 5: # 5 is magic number
                     possible_mails.append(i)
-    return get_unique_list(possible_mails)
-            
-
-#Testing
-
-'''      
-a = pickle.load(file('../sample.pickle'))
-mails = [a[0].folders_list[0][i] for i in range(628)]
-'''
-     
+    return (name, get_unique_list(possible_mails))
