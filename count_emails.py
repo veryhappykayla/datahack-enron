58a52bb5f7f064b72d5bbceaef4e5ebc853f5860
GGGapp <galsdh@gmail.com>
2015-11-26 16:47:52 +0200

    count emails is read :)

--- count_emails.py
+++ count_emails.py
@@ -4,6 +4,7 @@ from initial_data_parser import *
 import math
 import json
 from person_mapping import *
+import datetime
 
 def is_sent_by_user(Person, mail, person_dictionay):
     name = Person.name
@@ -29,6 +30,7 @@ def filter_mail_for_user_by_time(Person, start_date, end_date, person_dictionay)
                     new_mails.remove(mail)
         return new_mails
 
+#need to fix this function
 def group_mails_by_week(mails):
     mails = sorted(mails, key = lambda x: utils.dateParser(x.headers['Date']).toordinal())
     start_date = utils.dateParser(mails[0].headers['Date']).isocalendar()
@@ -37,21 +39,66 @@ def group_mails_by_week(mails):
     current_year = start_date[0]
     current_week = start_date[1]
     new_mails = []
-    for mail in mails:
-        iso_date = utils.dateParser(mail.headers['Date']).isocalendar()
+    for i in range(len(mails)):
+        iso_date = utils.dateParser(mails[i].headers['Date']).isocalendar()
         if iso_date[0] == current_year and iso_date[1] == current_week:
-            new_mails.append(mail)
+            new_mails.append(mails[i])
         else:
             ret_list.append((current_year, current_week, new_mails))
             new_mails = []
-            if current_week == 51:
-                current_week = 0
+            if current_week == 53:
+                current_week = 1
                 current_year += 1
             else:
                 current_week += 1
+            i -= 1
     return ret_list
-            
 
+def sort_mails(mails):
+    return sorted(mails, key = lambda x: utils.dateParser(x.headers['Date']).toordinal())
+
+def get_mail_by_day(date, mails):
+    ret_list = []
+    found_one = False
+    year, month, day = date.year, date.month, date.day
+    for i in range(len(mails)):
+        mail_date = utils.dateParser(mails[i].headers['Date'])
+        if mail_date.year == year and mail_date.month == month and mail_date.day == day:
+            ret_list.append(mails[i])
+            found_one = True
+        else:
+            if found_one == True:
+                return (i ,ret_list)
+    if found_one:
+        return (i, ret_list)
+    return (0, ret_list)
+
+
+def get_all_mails_for_all_days(mails):
+    mails = sort_mails(mails)
+    for i in range(len(mails)):
+        if utils.dateParser(mails[i].headers['Date']).year < 1998:
+            pass
+        else:
+            mails = mails[i:]
+            break
+    first_date = utils.dateParser(mails[0].headers['Date'])
+    last_date = utils.dateParser(mails[-1].headers['Date'])
+    ret_list = []
+    index = 0
+    
+    for d in range((last_date - first_date).days):
+        current_date = first_date + datetime.timedelta(d)
+        new_index, mm = get_mail_by_day(current_date, mails[index:])
+        index += new_index
+        #print index
+        ret_list.append((current_date, mm))
+    return ret_list
+
+def get_all_mails_for_all_days_for_user(user):
+    mails = user.get_all_emails()
+    return get_all_mails_for_all_days(mails)
+    
 WEEK_DAYS = {7:'Sun' , 1: 'Mon', 2:'Tue', 3:'Wed', 4:'Thu', 5 : 'Fri', 6: 'Sat'}
 
 def get_relevant_mails_from_user(Person, person_dictionay):
